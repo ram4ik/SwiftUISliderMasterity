197ddddf2a7747ec03e6f77bf27bd2654a961afb
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-03 18:53:41 +0200

    ColorPicker

--- SwiftUISliderMasterity/ContentView.swift
+++ SwiftUISliderMasterity/ContentView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var age: Double = 18
+    @State private var selectedColor: Color = Color.indigo
     
     var body: some View {
-        List {
-            Text("Selected Age - \(age, specifier: "%.0f")")
-            Slider(value: $age, in: 1...120) {
-                Text("Select Age")
-            } minimumValueLabel: {
-                Text("Min")
-            } maximumValueLabel: {
-                Text("Max")
+        VStack {
+            List {
+                Text("Selected Age - \(age, specifier: "%.0f")")
+                Slider(value: $age, in: 1...120) {
+                    Text("Select Age")
+                } minimumValueLabel: {
+                    Text("Min")
+                } maximumValueLabel: {
+                    Text("Max")
+                }
             }
-
+            
+            List {
+                ColorPicker("Select any color", selection: $selectedColor)
+            }
         }
     }
 }
