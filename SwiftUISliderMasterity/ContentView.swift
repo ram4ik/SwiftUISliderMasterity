92dafb6e49e8db4117e199c32b7cb9ba0961315d
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-03 18:41:47 +0200

    Initial Commit

---
+++ SwiftUISliderMasterity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISliderMasterity
+//
+//  Created by test on 03.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISliderMasterity/SwiftUISliderMasterityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderMasterityApp.swift
+//  SwiftUISliderMasterity
+//
+//  Created by test on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderMasterityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
